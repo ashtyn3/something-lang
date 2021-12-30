696ae5f56a8068f846aafc45cb11a548c85dc46c
ashtyn3 <ashtyn@gitmono.com>
2021-12-30 17:27:16 -0500

    fix: compile step child process is waited on before clean up

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -6,15 +6,15 @@ use std::process::Command;
 pub fn make_work(content: String, compile: bool) {
     let mut dir = env::temp_dir();
     dir.push("something_work");
-    fs::create_dir(&dir);
+    fs::create_dir(&dir).expect("Failed to create work directory");
 
     dir.push("module.cc");
-    fs::write(&dir, content);
+    fs::write(&dir, content).expect("Failed to write module");
     // g++ -Wall -o main main.cpp -static
     let mut current_dir = env::current_dir().unwrap();
     current_dir.push("som.out");
     if compile {
-        Command::new("g++")
+        let mut build = Command::new("g++")
             .args([
                 "-o",
                 current_dir.to_str().unwrap(),
@@ -23,11 +23,12 @@ pub fn make_work(content: String, compile: bool) {
             ])
             .spawn()
             .expect("Failed to build");
+        build.wait().expect("Failed to build");
     }
 }
 
 pub fn clean_work() {
     let mut dir = env::temp_dir();
     dir.push("something_work");
-    fs::remove_dir(dir).expect("Failed to clean up work directory");
+    fs::remove_dir_all(dir).expect("Failed to clean up work directory");
 }
