ecc6f474965240492e7eddfc457e31fda500c6f2
ashtyn3 <ashtyn@gitmono.com>
2022-01-30 14:03:48 -0500

    refactor(utils): Compile generated code seperate function

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -4,7 +4,7 @@ use std::process::{Command, Stdio};
 use std::env;
 use std::fs;
 
-pub fn make_work(content: String, compile: bool) {
+pub fn make_work(content: String) {
     let mut dir = env::temp_dir();
     dir.push("something_work");
     if dir.is_dir() == false {
@@ -14,20 +14,23 @@ pub fn make_work(content: String, compile: bool) {
     dir.push("module.cc");
     fs::write(&dir, content).expect("Failed to write module");
     // g++ -Wall -o main main.cpp -static
+}
+
+pub fn compile_gen() {
+    let mut dir = env::temp_dir();
+    dir.push("something_work");
     let mut current_dir = env::current_dir().unwrap();
     current_dir.push("som.out");
-    if compile {
-        let mut build = Command::new("g++")
-            .args([
-                "-o",
-                current_dir.to_str().unwrap(),
-                dir.to_str().unwrap(),
-                "-static",
-            ])
-            .spawn()
-            .expect("Failed to build");
-        build.wait().expect("Failed to build");
-    }
+    let mut build = Command::new("g++")
+        .args([
+            "-o",
+            current_dir.to_str().unwrap(),
+            dir.to_str().unwrap(),
+            "-static",
+        ])
+        .spawn()
+        .expect("Failed to build");
+    build.wait().expect("Failed to build");
 }
 
 pub fn clean_work() {
